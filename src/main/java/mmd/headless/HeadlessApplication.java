960395687e6a00713347bc8169ca2dd266e4afaa
Kyounghan Ryu <thankryu@gmail.com>
2023-05-09 18:02:07 +0900

    Initial commit

---
+++ src/main/java/mmd/headless/HeadlessApplication.java
@@ -0,0 +1,13 @@
+package mmd.headless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadlessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeadlessApplication.class, args);
+    }
+
+}
