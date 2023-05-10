08d4e0d896c73e652d0eb30ef9fc194bea722153
Kyounghan Ryu <thankryu@gmail.com>
2023-05-10 17:18:55 +0900

    #4 feat : 회원가입 중복 확인 로직 추가

--- src/main/java/mmd/headless/repository/MemberRepository.java
+++ src/main/java/mmd/headless/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package mmd.headless.repository;
 import mmd.headless.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsernameAndEmail(String username, String email);
 }
