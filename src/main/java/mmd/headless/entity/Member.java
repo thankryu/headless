790237c223288cc85d8d7211ee4459ed318f01e6
Kyounghan Ryu <thankryu@gmail.com>
2023-05-10 17:18:21 +0900

    #2 feat : Member builder 패턴으로 변경

--- src/main/java/mmd/headless/entity/Member.java
+++ src/main/java/mmd/headless/entity/Member.java
@@ -2,6 +2,7 @@ package mmd.headless.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import mmd.headless.enums.JoinType;
@@ -22,13 +23,11 @@ public class Member extends BaseEntity{
 
     private String username; // 사용자 이름
 
-    private String userId; // 사용자 아이디
+    private String email; // 사용자 이메일
 
     private String userPassword; // 사용자 비밀번호
 
     private String phone; // 사용자 전화번호
-
-    private String email; // 사용자 이메일
 
     private JoinType joinType; // 가입 타입
 
@@ -41,5 +40,14 @@ public class Member extends BaseEntity{
 
     public Member(String username) {
         this.username = username;
+    }
+
+    @Builder
+    public Member(String username, String email, String userPassword, String phone, JoinType joinType) {
+        this.username = username;
+        this.email = email;
+        this.userPassword = userPassword;
+        this.phone = phone;
+        this.joinType = joinType;
     }
 }
