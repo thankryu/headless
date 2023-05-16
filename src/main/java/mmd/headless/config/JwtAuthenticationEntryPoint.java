9fca1e25ab167acbf052a71bed7d833e00f0ef89
Kyounghan Ryu <thankryu@gmail.com>
2023-05-16 21:09:08 +0900

    #6 feat : 인증 실패 인터페이스 커스텀 추가

---
+++ src/main/java/mmd/headless/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package mmd.headless.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+        TokenFailDto dto = (TokenFailDto)request.getAttribute("unAuthorization");
+        String jsonStr = new ObjectMapper().writeValueAsString(dto);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(jsonStr);
+    }
+}
