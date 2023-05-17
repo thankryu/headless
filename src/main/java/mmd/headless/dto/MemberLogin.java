8766f3513a4abe16c0c78fab7e973848456b542e
Kyounghan Ryu <thankryu@gmail.com>
2023-05-17 15:31:42 +0900

    #8 feat : 로그인 api 정보 추가

--- src/main/java/mmd/headless/dto/MemberLogin.java
+++ src/main/java/mmd/headless/dto/MemberLogin.java
@@ -1,11 +1,20 @@
 package mmd.headless.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 public class MemberLogin {
+
+    @Schema(title = "이메일", example = "thankryu@gmail.com")
+    @NotEmpty(message = "이메일은 필수 입니다")
     public String email;
+
+    @Schema(title = "비밀번호", example = "1q2w3e4r5t")
+    @NotEmpty(message = "비밀번호는 필수입니다")
     public String password;
 }
