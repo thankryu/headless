1bebedfddb93541d1f3054a8d9eb239672741fe5
Kyounghan Ryu <thankryu@gmail.com>
2023-05-19 18:22:40 +0900

    #3 feat : 네이버 문자 발송 테스트 코드 추가

---
+++ src/test/java/mmd/headless/api/AlarmControllerTest.java
@@ -0,0 +1,68 @@
+package mmd.headless.api;
+
+import mmd.headless.dto.SmsRequest;
+import mmd.headless.dto.SignatureDto;
+import mmd.headless.service.AlarmServiceImpl;
+import mmd.headless.util.NaverUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+@SpringBootTest
+@Transactional
+class AlarmControllerTest {
+
+    @Value("${naver.access.key}") String accessKey;
+    @Value("${naver.service.id}") String serviceId;
+    @Value("${naver.sendNumber}") String sendNumber;
+    private String url = "/sms/v2/services/";
+
+    @Autowired
+    AlarmServiceImpl alarmService;
+
+    /**
+     * 네이버 발송 시그니처 키 생성 테스트
+     */
+    @Test
+    public void makeSignature() throws Exception {
+
+        String timestamp = String.valueOf(new Timestamp(System.currentTimeMillis()).getTime());
+
+        String keyUrl = url + serviceId +"/messages";
+
+        SignatureDto dto = SignatureDto.builder()
+                .method("POST")
+                .url(keyUrl)
+                .timestamp(timestamp)
+                .build();
+
+        String signature = NaverUtil.makeSignature(dto);
+        System.out.println("signature::"+signature);
+    }
+
+    /**
+     * 문자 발송 테스트
+     */
+    @Test
+    public void sendSMS() throws Exception{
+
+        SmsRequest form = new SmsRequest();
+        form.setType("SMS");
+        form.setFrom(sendNumber);
+        form.setSubject("문자 제목");
+        form.setContent("문자 내용");
+        form.setContentType("COMM");
+
+        SmsRequest.MessageRecipient messageRecipient = new SmsRequest.MessageRecipient();
+        messageRecipient.setTo("01000000000");
+        form.setMessages((Arrays.asList(messageRecipient)));
+
+        alarmService.sendSMS(form);
+    }
+
+}
