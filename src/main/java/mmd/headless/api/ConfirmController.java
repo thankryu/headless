f04420c35db17a81ea1e6c44567e996441521157
Kyounghan Ryu <thankryu@gmail.com>
2023-05-29 18:02:55 +0900

    #9 feat : 인증요청 api 파라미터 추가

--- src/main/java/mmd/headless/api/ConfirmController.java
+++ src/main/java/mmd/headless/api/ConfirmController.java
@@ -1,14 +1,18 @@
 package mmd.headless.api;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import mmd.headless.dto.ResultResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,10 +27,22 @@ public class ConfirmController {
             }
     )
     @PostMapping("/sendConfirm")
-    public ResultResponse sendConfirm(@RequestHeader("headLess") String headerValue){
+    public ResultResponse sendConfirm(@RequestHeader("headLess") String headerValue, @RequestBody ConfirmRequestDto dto){
 
         log.info("headerValue = "+headerValue);
 
         return new ResultResponse("", "success");
     }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class ConfirmRequestDto{
+
+        @Schema(title = "비밀번호", example = "010-0000-0000")
+        @NotEmpty(message = "전화번호는 필수입니다")
+        @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$",message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")
+        private String phone;
+    }
+
 }
